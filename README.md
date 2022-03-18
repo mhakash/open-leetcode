ec80497a1c24aa08ce178d5966dd695581b028f1
Mehdi Hassan Akash <mhakash211998@gmail.com>
2022-03-18 16:28:21 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Open leetcode
+
+A cli tool to open leetcode problems by number in your default browser
+
+### use without install
+
+```bash
+npx open-leetcode [problem number]
+```
+
+example `npx open-leetcode 12` will open 12 number problem
+
+OR
+
+### install
+
+```bash
+npm i -g open-leetcode
+```
+
+and use globally
+
+```
+open-leetcode 123
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const axios = require("axios");
+const child_process = require("child_process");
+
+const problemID = parseInt(process.argv[2]) || 1;
+
+let url = "https://leetcode.com/graphql";
+let query = `
+  query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {
+    problemsetQuestionList: questionList(categorySlug: $categorySlug limit: $limit skip: $skip filters: $filters) {
+      total: totalNum
+      questions: data {
+        frontendQuestionId: questionFrontendId
+        paidOnly: isPaidOnly
+        title
+        titleSlug
+      }
+    }
+  }
+`;
+
+let body = {
+  query: query,
+  variables: {
+    categorySlug: "",
+    skip: problemID - 1,
+    limit: 1,
+    filters: {},
+  },
+};
+
+(async () => {
+  const { data } = await axios.post(url, body);
+  const problemSlug = data.data.problemsetQuestionList.questions[0].titleSlug;
+  const urlToOpen = `https://leetcode.com/problems/${problemSlug}/`;
+
+  console.log(`Opening ${urlToOpen}`);
+
+  // open url in browser
+  let start =
+    process.platform == "darwin"
+      ? "open"
+      : process.platform == "win32"
+      ? "start"
+      : "xdg-open";
+
+  child_process.exec(`${start} "${urlToOpen}"`);
+})();
